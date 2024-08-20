ba517c3d3fb2512d312a4e265b4d3ee2889dec70
Rafael <casialdo@gmail.com>
2024-08-20 17:48:40 -0600

    Cloud GateWay

---
+++ src/main/java/com/example/cloud_gateway/CloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
